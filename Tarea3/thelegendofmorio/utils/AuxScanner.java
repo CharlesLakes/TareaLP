eb760cca2ff72a86c710ee3edd81c110b796fda1
CharlesLakes <carloslagoscortes@hotmail.com>
2021-11-12 20:31:35 -0300

    add comment

--- Tarea3/thelegendofmorio/utils/AuxScanner.java
+++ Tarea3/thelegendofmorio/utils/AuxScanner.java
@@ -3,5 +3,12 @@ package thelegendofmorio.utils;
 import java.util.Scanner;
 
 public class AuxScanner {
+    /**
+     * Esto es definido como auxiliar 
+     * para no tener problemas al tener instanciados
+     * muchos scanners y cerrarlos
+     * Ademas el editor me salia una alerta si no colocaba un close
+     * entonces esta es la forma mas ordenada.
+     */
     public static Scanner input = new Scanner(System.in);
 }
