eaf9d16910c7f8f7bfe151e91ba6cc884c2d7f1d
charleslakes <carloslagoscortes@hotmail.com>
2021-11-19 00:21:38 -0300

    Problema 3

--- Tarea4/problema3.rkt
+++ Tarea4/problema3.rkt
@@ -1,1 +1,40 @@
 #lang scheme
+
+;;(elevar numero exponente)
+;;Eleva un numero al exponente seleccionado
+;;Devuelve el numero elevado
+(define elevar
+	(lambda (numero exponente)
+	 (if (= exponente 0) 1 (* numero (elevar numero (- exponente 1))))
+	)
+ )
+
+;;(zeta_simple i s)
+;;Calcula la zeta simple
+;;Problema 3
+(define zeta_simple
+	(lambda (i s) 
+		(let loop-zeta-simple ((n 1) (r 0))
+			(if (= n (+ i 1))
+		    		r
+		    		(loop-zeta-simple (+ n 1) (+ r (/ 1 (elevar n s))))
+		 	)
+		)
+))
+
+;;(zeta_cola i s)
+;;Calcula la zeta cola
+;;Problema 3
+(define zeta_cola
+        (lambda (i s)
+                (let loop-zeta-cola ((n i) (r 0))
+                        (if (= n 0)
+                                r
+                                (loop-zeta-cola (- n 1) (+ r (/ 1 (elevar n s))))
+
+                        )
+                )
+))
+
+(zeta_simple 3 2)
+(zeta_cola 3 2)
