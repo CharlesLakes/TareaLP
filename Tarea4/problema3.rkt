c6ac24c2c46b4841344080db0c1bd5e78937fb72
charleslakes <carloslagoscortes@hotmail.com>
2021-11-23 17:28:57 -0300

    error problema 3

--- Tarea4/problema3.rkt
+++ Tarea4/problema3.rkt
@@ -1,14 +1,5 @@
 #lang scheme
 
-;;(elevar numero exponente)
-;;Eleva un numero al exponente seleccionado
-;;Devuelve el numero elevado
-(define elevar
-	(lambda (numero exponente)
-	 (if (= exponente 0) 1 (* numero (elevar numero (- exponente 1))))
-	)
- )
-
 ;;(zeta_simple i s)
 ;;Calcula la zeta simple
 ;;Retorna la sumatoria desde el inicio
@@ -16,12 +7,7 @@
 ;;Problema 3
 (define zeta_simple
 	(lambda (i s) 
-		(let loop-zeta-simple ((n 1) (r 0))
-			(if (= n (+ i 1))
-		    		r
-		    		(loop-zeta-simple (+ n 1) (+ r (/ 1 (elevar n s))))
-		 	)
-		)
+		(if (= i 1) 1 (+ (zeta_simple (- i 1) s) (/ 1 (expt i s))))
 ))
 
 ;;(zeta_cola i s)
@@ -34,7 +20,7 @@
                 (let loop-zeta-cola ((n i) (r 0))
                         (if (= n 0)
                                 r
-                                (loop-zeta-cola (- n 1) (+ r (/ 1 (elevar n s))))
+                                (loop-zeta-cola (- n 1) (+ r (/ 1 (expt n s))))
 
                         )
                 )
--- Tarea4/problema5.rkt
+++ Tarea4/problema5.rkt
@@ -1,1 +1,13 @@
 #lang scheme
+
+(define obtener-nodo-n (lambda (grafo n) 
+			(if (or (null? grafo) (= n (car (car grafo)))) (car grafo) (obtener-nodo-n (cdr grafo) n))
+		)
+  )
+
+(define contagio (lambda (grafo n d) 
+		   1
+		)
+  )
+
+(obtener-nodo-n  '((2 (1 3 4)) (1 (2)) (3 (2)) (4 (2))) 1)
