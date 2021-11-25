3b304f3017b98db01f34f9f777ecaa8f64102a28
charleslakes <carloslagoscortes@hotmail.com>
2021-11-25 00:43:10 -0300

    problema 4

--- Tarea4/problema4.rkt
+++ Tarea4/problema4.rkt
@@ -7,18 +7,20 @@
 ;;Problema 4
 (define vida 
 	(lambda (h arbol)
-		(let loop-arbol ((nodo (car arbol)) (arbol-izquierdo (cadr arbol)) (arbol-derecho (caddr arbol)) (lista-nodos '()))
-		  	(cond
-			  	[(= h nodo) lista-nodos]
-			 	[(and (not (null? arbol-izquierdo)) (not (null? (loop-arbol (car arbol-izquierdo) (cadr arbol-izquierdo) (caddr arbol-izquierdo) (append lista-nodos (list nodo))))))
-				 (loop-arbol (car arbol-izquierdo) (cadr arbol-izquierdo) (caddr arbol-izquierdo) (append lista-nodos (list nodo)))]
-				[(and (not (null? arbol-derecho)) (not (null? (loop-arbol (car arbol-derecho) (cadr arbol-derecho) (caddr arbol-derecho) (append lista-nodos (list nodo)))))) 
-				 (loop-arbol (car arbol-derecho) (cadr arbol-derecho) (caddr arbol-derecho) (append lista-nodos (list nodo)))]
-				[else '()]
+	  	(if (null? arbol) arbol
+			(let loop-arbol ((nodo (car arbol)) (arbol-izquierdo (cadr arbol)) (arbol-derecho (caddr arbol)) (lista-nodos '()))
+		  		(cond
+			  		[(= h nodo) lista-nodos]
+			 		[(and (not (null? arbol-izquierdo)) (not (null? (loop-arbol (car arbol-izquierdo) (cadr arbol-izquierdo) (caddr arbol-izquierdo) (append lista-nodos (list nodo))))))
+				 		(loop-arbol (car arbol-izquierdo) (cadr arbol-izquierdo) (caddr arbol-izquierdo) (append lista-nodos (list nodo)))]
+					[(and (not (null? arbol-derecho)) (not (null? (loop-arbol (car arbol-derecho) (cadr arbol-derecho) (caddr arbol-derecho) (append lista-nodos (list nodo)))))) 
+				 		(loop-arbol (car arbol-derecho) (cadr arbol-derecho) (caddr arbol-derecho) (append lista-nodos (list nodo)))]
+					[else '()]
 
 
-			 )
-		  )
+			 	)
+		  	)
+		)
 
 	 )
 
